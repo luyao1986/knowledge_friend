112a47ed65476b547c5fc366374bccd5b8eba931
luyao1986 <qian_198697@163.com>
2016-04-22 23:09:24 +0200

    using inline css(radium) to set knowledge color

--- imports/ui/components/Knowledge.jsx
+++ imports/ui/components/Knowledge.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Col, Thumbnail, Button, ProgressBar } from 'react-bootstrap';
 import { setKnowledgeReviewed } from '../../api/knowledgelist/methods.js';
+import { Radium } from 'radium';
 
 export class Knowledge extends React.Component {
   constructor(props) {
@@ -11,11 +12,11 @@ export class Knowledge extends React.Component {
     };
   render() {
       const stage2progress = [0, 0, 15, 30, 45, 60, 75, 90, 100];
-      // const stage2color = ["#ff0000", "#ff9b00", "#ffff00", "#00ff00", "#0000ff", "#00ffff", "#ff00ff", "#ffffff"];
+      const stage2color = ["#ff0000", "#ff9b00", "#ffff00", "#00ff00", "#0000ff", "#00ffff", "#ff00ff", "#ffffff"];
       return (
           <Col xs={12} md={6}>
               <Thumbnail responsive>
-                  <h3 className={stage2color[this.props.content.stage]}>{this.props.content.title}</h3>
+                  <h3 style={{color: stage2color[this.props.content.stage]}}>{this.props.content.title}</h3>
                   <p>{this.props.content.description}</p>
                   <ProgressBar bsStyle="success" now={stage2progress[this.props.content.stage]}></ProgressBar>
                   <p><Button bsStyle="primary" onClick={this.setReviewed.bind(this, this.props.content)}>Review</Button></p>
