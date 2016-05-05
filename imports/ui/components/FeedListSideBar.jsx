d709ffea02fab70b927fa9ac8e91dd5dfaaee0dd
luyao1986 <qian_198697@163.com>
2016-05-05 20:50:30 +0200

    parse response correctly

--- imports/ui/components/FeedListSideBar.jsx
+++ imports/ui/components/FeedListSideBar.jsx
@@ -11,12 +11,16 @@ export class FeedListSideBar extends React.Component {
   };
 
   callback(data) {
-    let newfeed = {category:data['responseData'].title, url:data['responseData'].url};
+    let newfeed = {category: data['responseData']['feed'].title, url: data['responseData']['feed'].feedUrl};
     console.log("insert new feed: ", newfeed);
     insertFeed.call(newfeed);
+    console.log("in callback:", this);
     this.props.updateState(newfeed);
   };
 
+  componentDidMount() {
+    console.log("componentDidMount:", this.props);
+  }
   addNewFeed(e) {
     e.preventDefault();
     let url = `${this.refs.feedinput.value}/feed.xml`;
@@ -24,6 +28,7 @@ export class FeedListSideBar extends React.Component {
     this.refs.feedinput.value = "";
   };
   render() {
+    console.log("render:", this.props);
     return (
       <div>
         <form onSubmit={this.addNewFeed.bind(this)}>
